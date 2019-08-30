f21a99a99d3b73e5d0ff263c593e937c078b8849
Sean McCarthy <seanmccarthy3223@GMAIL.com>
2019-08-30 18:05:20 -0500

    setup basic proxy server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const PORT = 3003;
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Proxy is live on port ${PORT}!`)
+});
