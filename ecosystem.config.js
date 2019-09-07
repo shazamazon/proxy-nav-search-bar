858315ac7924e56b61316db4a2d4386df30ef9d7
Sean McCarthy <seanmccarthy3223@GMAIL.com>
2019-09-07 15:39:51 -0500

    add pm2 for deployment and deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'Proxy-Nav',
+    script: './server/index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-218-185-210.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/FEC-first-key.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:shazamazon/proxy-nav-search-bar.git',
+      path: '/home/ubuntu/proxy-nav-search-bar',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+};
