1dc3d7a2b714ae14c8642b2c25605f2b2c3bd681
Sean McCarthy <seanmccarthy3223@GMAIL.com>
2019-09-07 15:49:31 -0500

    bugfix with deployment #3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       key: '~/.ssh/FEC-first-key.pem',
       ref: 'origin/master',
       repo: 'git@github.com:shazamazon/proxy-nav-search-bar.git',
-      path: '/home/ubuntu/',
+      path: '/home/ubuntu',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
