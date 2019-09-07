f0b63418249f8a4efda24fc498f27af5109cb153
Sean McCarthy <seanmccarthy3223@GMAIL.com>
2019-09-07 15:10:59 -0500

    create actual proxy

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
-const PORT = 80;
+const PORT = 3000;
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/../public'));
 app.use(bodyParser.json());
 
 app.listen(PORT, () => {
-  console.log(`Proxy is live on port ${PORT}!`)
+  console.log(`Proxy is live on port ${PORT}!`);
 });
